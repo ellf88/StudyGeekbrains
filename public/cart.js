1c808507626c738eaefd02454cfef95c7757a8ea
ellf88 <ellf88@mail.ru>
2019-04-01 15:23:51 +0300

    Код по корзине, сделал вывод с сервера выбранных товаров и их удаление нажатием по кнопке. Правда удаляется все количество выбранного товара. Не стал добавлять кнопки минус количества.

--- public/cart.js
+++ public/cart.js
@@ -1,9 +1,16 @@
+let goodsRequest = (url) => {
+   return fetch(url).then((response) => response.json());
+};
+
+const API = "http://localhost:3000";
+
 class CartItem {
     constructor(product, img = 'https://www.resursltd.ru/images/cms/thumbs/f010964dc04aea08ebcc6ded3d85364e36d15396/nofoto_120_100_5_100.jpg') {
         this.id = product.id;
         this.title = product.title;
         this.price = product.price;
         this.img = img;
+        this.quantity = product.quantity;
     }
 
     render() {
@@ -12,8 +19,60 @@ class CartItem {
                 <img class="img" src="${this.img}" alt="img">
                 </div>
                 <h3 class="description">${this.title}</h3>
-                <h3 class="price">$${this.price}</h3>
-                <button class="addToCart">Удалить из корины</button>
+                <h3 class="price">$${this.price} x ${this.quantity} = ${this.price * this.quantity}</h3>
+                <button class="addToCart" data-id="${this.id}">Удалить из корины</button>
             </div>`
     }
 }
+
+class ItemsList {
+    constructor(container = ".container") {
+        this.container = container;
+        this.products = [];
+    }
+
+    fetchGoods() {
+        let goods = [];
+        goodsRequest(`${API}/cartGoods`)
+            .then(data => {
+                goods = data;
+                this._render(goods)
+            })
+    }
+
+    removeItem() {
+        let $container = document.querySelector(this.container);
+        $container.addEventListener("click", (event) =>{
+            if(event.target.classList.contains("addToCart")) {
+                if(confirm("Точна?")) {
+                    const id = +event.target.dataset.id;
+                    fetch(`${API}/cartGoods/${id}`, {method: "DELETE"})
+                        .then(() => {
+                            $container.removeChild(event.target.parentElement);
+                        })
+                }
+            }
+        })
+    }
+
+    _render(goods) {
+        const block = document.querySelector(this.container);
+            for (let item of goods) {
+                const itemHtml = new CartItem(item);
+                this.products.push(itemHtml);
+                block.insertAdjacentHTML("beforeend", itemHtml.render());
+            }
+    }
+}
+
+let itemsCart = new ItemsList();
+itemsCart.fetchGoods();
+itemsCart.removeItem();
+
+
+
+
+
+
+
+
